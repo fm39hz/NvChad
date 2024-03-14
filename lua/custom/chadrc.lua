3b79d9017324b87b29768dde1a10fe310c932157
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-03-14 13:35:00 +0700

    feat: config default nvchad ui

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -6,8 +6,17 @@ local highlights = require "custom.highlights"
 
 M.ui = {
   theme = "yoru",
-  theme_toggle = { "yoru", "yoru" },
-
+  theme_toggle = { "yoru", "gruvchad" },
+  telescope = {
+    style = "bordered"
+  },
+  nvdash = {
+    load_on_startup = true
+  },
+  statusline = {
+    theme = "vscode_colored",
+    separator_style = "round"
+  },
   hl_override = highlights.override,
   hl_add = highlights.add,
 }
