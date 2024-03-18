06524f6177cacc08b48c4602fc54e8ecc4682420
fm39hz-desktop <hitpoint2k3@gmail.com>
2024-03-19 00:48:38 +0700

    feat: implement essentials keymap

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -3,8 +3,7 @@ local M = {}
 
 M.general = {
   n = {
-    [":"] = { ":", "enter command mode", opts = { nowait = true } },
-    [";"] = {"<cmd> lua require('fine-cmdline').open({default_value = ''}) <CR>"},
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<leader>gg"] = {"<cmd> LazyGit <CR> "}
   },
 }
@@ -13,11 +12,29 @@ M.disabled = {
 -- more keybinds!
 M.debug = {
   n = {
-    ["<leader>b"] = {"<cmd> lua require('custom.configs.dapconfig').toggle_breakpoint() <CR>"},
-    ["<f5>"] = {"<cmd> lua require('custom.configs.dapconfig').continue() <CR>"},
-    ["<f10>"] = {"<cmd> lua require('custom.configs.dapconfig').step_over() <CR>"},
-    ["<f11>"] = {"<cmd> lua require('custom.configs.dapconfig').step_into() <CR>"},
+    ["<leader>b"] = {"<cmd> lua require('custom.configs.dapconfig').toggle_breakpoint() <CR>", desc = "Toggle breakpoint"},
+    ["<f5>"] = {"<cmd> lua require('custom.configs.dapconfig').continue() <CR>", desc = "Continue"},
+    ["<f10>"] = {"<cmd> lua require('custom.configs.dapconfig').step_over() <CR>", desc = "Step over"},
+    ["<f11>"] = {"<cmd> lua require('custom.configs.dapconfig').step_into() <CR>", desc = "Step into"},
 
   },
 }
+M.window = {
+  n = {
+    ["<C-k>"] = {"<cmd> resize +2 <cr>", desc = "Increase window height"},
+    ["<C-j>"] = {"<cmd> resize -2 <cr>", desc = "Decrease window height"},
+    ["<C-l>"] = {"<cmd> vertical resize -2 <cr>", desc = "Decrease window width"},
+    ["<C-h>"] = {"<cmd> vertical resize +2 <cr>", desc = "Increase window width"},
+    }
+  }
+M.moveLines = {
+  n = {
+    ["<A-k>"] = {"<cmd>m .-2<cr>==", desc = "Move line up"},
+    ["<A-j>"] = {"<cmd>m .+1<cr>==", desc = "Move line down"},
+    },
+  i = {
+    ["<A-k>"] = {"<esc><cmd>m .-2<cr>==gi", desc = "Move line up"},
+    ["<A-j>"] = {"<esc><cmd>m .+1<cr>==gi", desc = "Move line down"},
+  }
+  }
 return M
