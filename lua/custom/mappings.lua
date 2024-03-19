43e69bff0ff27fb89d7554fb450c428a03512fc8
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-03-19 23:03:38 +0700

    feat: add lsp function in F row

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -15,26 +15,44 @@ M.code = {
     ["<leader>b"] = {
       function ()
         require('custom.configs.dapconfig').toggle_breakpoint()
-      end
-      , "Toggle breakpoint"
+      end,
+      "Toggle breakpoint"
+    },
+    ["<f2>"] = {
+      function ()
+        vim.lsp.buf.rename()
+      end,
+      "Rename current symbol"
     },
     ["<f5>"] = {
       function ()
         require('custom.configs.dapconfig').continue()
-      end
-      , "Continue"
+      end,
+      "Continue"
     },
     ["<f10>"] = {
       function ()
         require('custom.configs.dapconfig').step_over()
-      end
-      , "Step over"
+      end,
+      "Step over"
     },
     ["<f11>"] = {
       function ()
         require('custom.configs.dapconfig').step_into()
-      end
-      , "Step into"
+      end,
+      "Step into"
+    },
+    ["<f12>"] = {
+      function ()
+        vim.lsp.buf.definition()
+      end,
+      "Show the definition of current symbol"
+    },
+    ["<F12>"] = {
+      function ()
+        vim.lsp.buf.declaration()
+      end,
+      "Show the declaration of current symbol"
     },
   },
 }
