add619b373b25458461b631cbdee41f58441f99b
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-03-19 11:30:17 +0700

    feat: implement description

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -4,36 +4,36 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<leader>gg"] = {"<cmd> LazyGit <CR> "}
   },
 }
 M.disabled = {
 }
 -- more keybinds!
-M.debug = {
+M.code = {
   n = {
-    ["<leader>b"] = {"<cmd> lua require('custom.configs.dapconfig').toggle_breakpoint() <CR>", desc = "Toggle breakpoint"},
-    ["<f5>"] = {"<cmd> lua require('custom.configs.dapconfig').continue() <CR>", desc = "Continue"},
-    ["<f10>"] = {"<cmd> lua require('custom.configs.dapconfig').step_over() <CR>", desc = "Step over"},
-    ["<f11>"] = {"<cmd> lua require('custom.configs.dapconfig').step_into() <CR>", desc = "Step into"},
+    ["<leader>gg"] = {"<cmd> LazyGit <CR>"},
+    ["<leader>b"] = {"<cmd> lua require('custom.configs.dapconfig').toggle_breakpoint() <CR>", "Toggle breakpoint"},
+    ["<f5>"] = {"<cmd> lua require('custom.configs.dapconfig').continue() <CR>", "Continue"},
+    ["<f10>"] = {"<cmd> lua require('custom.configs.dapconfig').step_over() <CR>", "Step over"},
+    ["<f11>"] = {"<cmd> lua require('custom.configs.dapconfig').step_into() <CR>", "Step into"},
   },
 }
 M.window = {
   n = {
-    ["<A-K>"] = {"<cmd> resize +2 <cr>", desc = "Increase window height"},
-    ["<A-J>"] = {"<cmd> resize -2 <cr>", desc = "Decrease window height"},
-    ["<A-L>"] = {"<cmd> vertical resize -2 <cr>", desc = "Decrease window width"},
-    ["<A-H>"] = {"<cmd> vertical resize +2 <cr>", desc = "Increase window width"},
-    }
+    ["<A-K>"] = {"<cmd> resize +2 <cr>", "Increase window height"},
+    ["<A-J>"] = {"<cmd> resize -2 <cr>", "Decrease window height"},
+    ["<A-L>"] = {"<cmd> vertical resize -2 <cr>", "Decrease window width"},
+    ["<A-H>"] = {"<cmd> vertical resize +2 <cr>", "Increase window width"},
   }
+}
 M.moveLines = {
   n = {
-    ["<A-k>"] = {"<cmd>m .-2<cr>==", desc = "Move line up"},
-    ["<A-j>"] = {"<cmd>m .+1<cr>==", desc = "Move line down"},
-    },
+    ["<A-k>"] = {"<cmd>m .-2<cr>==", "Move line up"},
+    ["<A-j>"] = {"<cmd>m .+1<cr>==", "Move line down"},
+  },
   i = {
-    ["<A-k>"] = {"<esc><cmd>m .-2<cr>==gi", desc = "Move line up"},
-    ["<A-j>"] = {"<esc><cmd>m .+1<cr>==gi", desc = "Move line down"},
-    }
+    ["<A-k>"] = {"<esc><cmd>m .-2<cr>==gi", "Move line up"},
+    ["<A-j>"] = {"<esc><cmd>m .+1<cr>==gi", "Move line down"},
   }
+}
 return M
